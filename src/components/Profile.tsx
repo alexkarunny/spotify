9848aca979a62cc73106e9c04a53dff6fda4301f
alexkarunny <smetan4ik1@gmail.com>
2021-12-16 17:57:45 +0300

    feat: get tracks

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,37 +1,72 @@
 import React, {useEffect, useState} from "react";
-import axios from "axios";
+import axios, {AxiosResponse} from "axios";
+
+type PlaylistType = {
+    id: string
+    name: string
+}
+
+type TrackType = {
+    artist: string
+    title: string
+}
+
+type TracksStateType = {
+    [key: string] : TrackType[]
+}
 
 export function Profile() {
 
-    const [accessToken, setAccessToken] = useState<string | null>(null)
+    const [accessToken, setAccessToken] = useState<string>()
+    const [playlists, setPlaylists] = useState<PlaylistType[]>([])
+    const [tracks, setTracks] = useState()
 
     useEffect(() => {
-        setInterval(() => {
-            const hash = window.location.hash
-            const startPosition = hash.indexOf('=')
-            const lastPosition = hash.indexOf('&')
-            setAccessToken(hash.substring(startPosition + 1, lastPosition))
-        }, 3600)
+        const hash = window.location.hash
+        const startPosition = hash.indexOf('=')
+        const lastPosition = hash.indexOf('&')
+        const token = hash.substring(startPosition + 1, lastPosition)
+
+        axios.get('https://api.spotify.com/v1/me/playlists', {
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        }).then((res: AxiosResponse) => {
+            debugger
+            const playlists = res.data.items.map((item: any): PlaylistType => {
+                return {id: item.id, name: item.name}
+            })
+            debugger
+            setPlaylists(playlists)
+            setAccessToken(token)
+        })
+
     }, [])
 
-    const getUserInfo = () => {
-        if (accessToken) {
-            axios.get('https://api.spotify.com/v1/me/playlists', {
+    const getTracks = () => {
+        debugger
+        let tracksState: TracksStateType = {}
+
+        playlists.map((playlist) => {
+            axios.get(`https://api.spotify.com/v1/playlists/${playlist.id}/tracks`, {
                 headers: {
                     'Authorization': 'Bearer ' + accessToken
                 }
-            }).then((res) => {
-               debugger
+            }).then(res => {
+                debugger
+                tracksState[playlist.id] = res.data.items.map((item: any): TrackType => {
+                    return {title: item.track.name, artist: item.track.artists[0].name}
+                })
+                debugger
             })
-        }
+        })
+
     }
 
     return (
         <div>
-
-            <button onClick={getUserInfo}>Get User Info</button>
-
-            <div>{accessToken}</div>
+            <button onClick={getTracks}>Get User Info</button>
+            <div>{tracks}</div>
         </div>
     )
 }
