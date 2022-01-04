a303f703c2ae87fb651e021e899c9efe5f9d9dba
alexkarunny <smetan4ik1@gmail.com>
2022-01-04 17:47:05 +0300

    feat: sessionStorage

--- src/api/playlist-api.ts
+++ src/api/playlist-api.ts
@@ -1,14 +1,17 @@
 import axios from "axios";
 
+debugger
+
 const hash = window.location.hash
 const startPosition = hash.indexOf('=')
 const lastPosition = hash.indexOf('&')
 const token = hash.substring(startPosition + 1, lastPosition)
+sessionStorage.setItem('token', token)
 
 const instance = axios.create({
     baseURL: 'https://api.spotify.com/v1/',
     headers: {
-        'Authorization': 'Bearer ' + token
+        'Authorization': 'Bearer ' + sessionStorage.getItem('token')
     }
 })
 
